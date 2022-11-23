c8224937ea96e146fa2140eb551b353c8c17ea31
Aman Kumar <aman.asingh307@gmail.com>
2022-11-23 13:47:37 +0530

    Added Movie Search Functionality

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask,render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+import urllib
+import json
+import random
 
 app = Flask(__name__)
 
@@ -61,12 +64,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
     
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/login',methods = ['GET','POST'])
+@app.route('/',methods = ['GET','POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -78,11 +76,43 @@ def login():
             
     return render_template('login.html',form = form)
 
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    print("HELLO I AM A FLASK APP")
+    movieName = ["Avengers","Avatar"]
+    myMovieName = random.choice(movieName)
+    try:
+        url =  f"http://www.omdbapi.com/?s={myMovieName}&apikey=3153a9ba"
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        jsonData = json.loads(data)["Search"]
+        return render_template("dashboard.html" , page_name = "WHAT2WATCH" , movieList = jsonData)
+    except Exception as e:
+        
+        return render_template("dashboard.html" , page_name = "WHAT2WATCH")
 
+
+@app.route("/search",methods=['Get'])
+def search_results():
+    movieName = request.args.get("query")
+    try:
+        url =  f"http://www.omdbapi.com/?s={movieName}&apikey=3153a9ba"
+        url = url.replace(" ", "%20")
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        jsonData = json.loads(data)["Search"]
+        return render_template("dashboard.html" , page_name = "SEARCH RESULTS" , movieList = jsonData,myquery = movieName)
+    except Exception as e:
+        print(e)
+        return f"NO INTERNET CONNECTION {e}"
+
+
+'''
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
+'''
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
